e2e3df6b92c976480bebf6c7b1cbaac281c62da8
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-05-31 20:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.46%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -12,23 +12,42 @@ class Solution:
         # return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
         
 
-        stack1 = []
-        stack2 = []
-        if root : 
-            stack1.append(root)
-        else : 
-            return []
-        while stack1 :
-            node = stack1.pop()
-            stack2.append(node)
-            if node.left :
-                stack1.append(node.left)
-            if node.right :
-                stack1.append(node.right)
+        # stack1 = []
+        # stack2 = []
+        # if root : 
+        #     stack1.append(root)
+        # else : 
+        #     return []
+        # while stack1 :
+        #     node = stack1.pop()
+        #     stack2.append(node)
+        #     if node.left :
+        #         stack1.append(node.left)
+        #     if node.right :
+        #         stack1.append(node.right)
+
+        # res = []
+        # for i in range(len(stack2)-1,-1,-1) :
+        #     res.append(stack2[i].val)
+        # return res
 
         res = []
-        for i in range(len(stack2)-1,-1,-1) :
-            res.append(stack2[i].val)
+        stack = [root]
+        visit = [False]
+
+        while stack :
+            curr , v = stack.pop() , visit.pop()
+            if curr :
+                if v :
+                    res.append(curr.val)
+                else :
+                    stack.append(curr)
+                    visit.append(True)
+                    stack.append(curr.right)
+                    visit.append(False)
+                    stack.append(curr.left)
+                    visit.append(False)
+
         return res
 
 
