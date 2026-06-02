9f395617815ae6af9d0700e4f9d78c1d61c619fd
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-02 06:31:42 +0530

    Time: 395 ms (6.05%) | Memory: 20.2 MB (99.19%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -5,29 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    import math
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root :
-            return True
+        # if not root :
+        #     return True
 
-        def depth(root) :
+        # def depth(root) :
+        #     if not root :
+        #         return 0 
+        #     elif not root.left and not root.right :
+        #         return 1 
+        #     else :
+        #         return 1 + max(depth(root.left), depth(root.right))  
+
+        # lh = depth(root.left)
+        # rh = depth(root.right)
+        # if abs(lh-rh) > 1 :
+        #     return False 
+        # left = self.isBalanced(root.left)
+        # right = self.isBalanced(root.right)
+
+        # if not left or not right :
+        #     return False 
+        # return True
+        def dfsHeight(root) :
             if not root :
                 return 0 
-            elif not root.left and not root.right :
-                return 1 
-            else :
-                return 1 + max(depth(root.left), depth(root.right))  
+            lh = dfsHeight(root.left)
+            rh = dfsHeight(root.right)
+            if lh == -1 or rh == -1 : 
+                return -1 
+            if abs(lh - rh) > 1 :
+                return -1 
+            return max(dfsHeight(root.right),dfsHeight(root.left)) + 1 
 
-        lh = depth(root.left)
-        rh = depth(root.right)
-        if abs(lh-rh) > 1 :
-            return False 
-        left = self.isBalanced(root.left)
-        right = self.isBalanced(root.right)
-
-        if not left or not right :
-            return False 
-        return True
+        return False if dfsHeight(root) == -1 else True
 
 
 
