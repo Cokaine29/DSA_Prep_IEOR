85fe67be0adabb41b8ca2255341f89fe051045f5
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-04 09:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.46%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -31,24 +31,56 @@ class Solution:
         #     res.append(stack2[i].val)
         # return res
 
-        res = []
-        stack = [root]
-        visit = [False]
+        # Using 1 stack
 
+        # res = []
+        # stack = [root]
+        # visit = [False]
+
+        # while stack :
+        #     curr , v = stack.pop() , visit.pop()
+        #     if curr :
+        #         if v :
+        #             res.append(curr.val)
+        #         else :
+        #             stack.append(curr)
+        #             visit.append(True)
+        #             stack.append(curr.right)
+        #             visit.append(False)
+        #             stack.append(curr.left)
+        #             visit.append(False)
+
+        # return res
+
+        ## All Three Traversals using only 1 stack
+
+        preorder = []
+        inorder = []
+        postorder = []
+
+        if not root :
+            return []
+
+        stack = []
+        stack.append([root, 1])
         while stack :
-            curr , v = stack.pop() , visit.pop()
-            if curr :
-                if v :
-                    res.append(curr.val)
-                else :
-                    stack.append(curr)
-                    visit.append(True)
-                    stack.append(curr.right)
-                    visit.append(False)
-                    stack.append(curr.left)
-                    visit.append(False)
-
-        return res
+            node = stack.pop()
+            if node[1] == 1 :
+                preorder.append(node[0].val)
+                node[1] += 1 
+                stack.append(node)
+                if node[0].left :
+                    stack.append([node[0].left, 1])
+            elif node[1] == 2 :
+                inorder.append(node[0].val)
+                node[1] += 1 
+                stack.append(node)
+                if node[0].right :
+                    stack.append([node[0].right, 1])
+            elif node[1] == 3 :
+                postorder.append(node[0].val)
+        return postorder
+                
 
 
 
