c6ae0eadb79585242dac96a9f4764968ca6f0146
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-08 12:53:16 +0530

    Time: 4 ms (32.18%) | Memory: 19.4 MB (8.86%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
@@ -1,11 +1,11 @@
 class Solution:
     def secondHighest(self, s: str) -> int:
-        d = {"0","1","2","3","4","5","6","7","8","9"}
+        # d = {"0","1","2","3","4","5","6","7","8","9"}
         m1 = -1
         m2 = -1 
 
         for i in s :
-            if i in d :
+            if i.isdigit() :
                 if int(i) > m1 :
                     m2 = m1 
                     m1 = int(i) 
