7a48e02cbb5366b61da28d0d0fbc32d4f01d8d10
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-09 20:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.50%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -3,20 +3,41 @@ class Solution:
 
         ## Brute
 
-        L = len(nums) 
+        # L = len(nums) 
+        # if L == 1 :
+        #     return True
+        # prev = nums[0]
+        # pivot = 0 
+        # for i in range(L) :
+        #     if nums[i] < prev :
+        #         pivot = i
+        #         break
+        #     prev = nums[i] 
+        # sorted = nums[pivot:] + nums[:pivot]
+        # # print(sorted)
+        # for i in range(1,len(sorted)) :
+        #     if sorted[i] < sorted[i-1] :
+        #         return False 
+        # return True
+
+
+        ## Optimal
+
+        L = len(nums)
         if L == 1 :
             return True
-        prev = nums[0]
-        pivot = 0 
-        for i in range(L) :
-            if nums[i] < prev :
-                pivot = i
-                break
-            prev = nums[i] 
-        sorted = nums[pivot:] + nums[:pivot]
-        # print(sorted)
-        for i in range(1,len(sorted)) :
-            if sorted[i] < sorted[i-1] :
-                return False 
-        return True
+        count = 0 
+        new = nums[:] + nums[:]
+        i = 0 
+        j = 1 
+        while i < 2*L and j < 2*L:
+            while j < 2*L and new[j] >= new[j-1] :
+                j += 1 
+            if j - i >= L :
+                return True
+            else :
+                i = j 
+                j += 1
+        return False
+
         
