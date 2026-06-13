02196030820a273887713cb8a1e3ba94c31f9a33
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-13 09:25:06 +0530

    Time: 7 ms (38.02%) | Memory: 19.1 MB (82.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,24 +1,39 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        ## Approach 1 
 
-        l1 = len(s)
-        l2 = len(t) 
+        # l1 = len(s)
+        # l2 = len(t) 
 
-        d = {}
-        rhs = set() 
+        # d = {}
+        # rhs = set() 
 
-        for i in range(l1) :
-            if s[i] in d :
-                if d[s[i]] == t[i] :
-                    pass
-                else :
-                    return False
+        # for i in range(l1) :
+        #     if s[i] in d :
+        #         if d[s[i]] == t[i] :
+        #             pass
+        #         else :
+        #             return False
+        #     else :
+        #         if t[i] in rhs :
+        #             return False 
+        #         else :
+        #             d[s[i]] = t[i] 
+        #             rhs.add(t[i])
+        # return True
+
+        ## Approach 2 
+
+        m1 = [0] * 256
+        m2 = [0] * 256 
+
+        for i in range(len(s)) :
+            if m1[ord(s[i])] == m2[ord(t[i])] :
+                m1[ord(s[i])] = i + 1 
+                m2[ord(t[i])] = i + 1
             else :
-                if t[i] in rhs :
-                    return False 
-                else :
-                    d[s[i]] = t[i] 
-                    rhs.add(t[i])
-        return True
+                return False 
+        return True 
 
         
