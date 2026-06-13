471c431dc533b2b3ccb0b6b96950776b9ebe4412
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-13 14:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (49.58%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,16 +1,31 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
 
-        stack = []
-        count = 0  
+        # stack = []
+        # count = 0  
+        # ans = 0
+        # for ele in s :
+        #     if ele == "(" :
+        #         stack.append(ele)
+        #         count = len(stack)
+        #         ans = max(count,ans)
+        #     elif ele == ")" : 
+        #         stack.pop()
+        # return ans 
+
+        count = 0
         ans = 0
         for ele in s :
             if ele == "(" :
-                stack.append(ele)
-                count = len(stack)
+                count += 1 
                 ans = max(count,ans)
-            elif ele == ")" : 
-                stack.pop()
+            elif ele == ")" :
+                count -= 1 
+            else :
+                pass
+
         return ans 
+
+
             
         
