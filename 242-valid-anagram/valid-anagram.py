d8c611919d6b8244abe1cbaaca086a7f8ebd3e1e
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-13 10:05:03 +0530

    Time: 15 ms (43.01%) | Memory: 19.3 MB (75.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,25 +3,25 @@ class Solution:
         
         # # Approach 1 
 
-        # d = {}
+        d = {}
 
-        # for c in s :
-        #     if c in d :
-        #         d[c] += 1 
-        #     else :
-        #         d[c] = 1 
+        for c in s :
+            if c in d :
+                d[c] += 1 
+            else :
+                d[c] = 1 
         
-        # for c in t :
-        #     if c in d :
-        #         d[c] -= 1 
-        #     else :
-        #         return False 
-        # for ele in d :
-        #     if d[ele] != 0 :
-        #         return False 
-        # return True
+        for c in t :
+            if c in d :
+                d[c] -= 1 
+            else :
+                return False 
+        for ele in d :
+            if d[ele] != 0 :
+                return False 
+        return True
 
         ## Approach 2 
 
 
-        return True if sorted(s) == sorted(t) else False         
+        # return True if sorted(s) == sorted(t) else False         
