3ff10f295c1b29f4cbb39209df0d004a2507b0b8
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-16 21:35:38 +0530

    Time: 65 ms (5.09%) | Memory: 22.5 MB (85.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -35,5 +35,5 @@ class Solution:
                 slow = slow.next
                 fast = fast.next.next
         
-        if not fast or not fast.next or not fast.next.next :
+        if not fast or not fast.next:
             return False 
