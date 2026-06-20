a97df522dfaa50d16679a2c9cdcdabac23c6704e
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-20 10:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.84%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -8,19 +8,35 @@ class Solution:
 
         ## BruteForce 
 
-        count = 0 
-        curr = head 
-        while curr :
-            count += 1 
-            curr = curr.next
-        if count == 1 and n == 1 :
+        # count = 0 
+        # curr = head 
+        # while curr :
+        #     count += 1 
+        #     curr = curr.next
+        # if count == 1 and n == 1 :
+        #     return None
+        # elif count == n :
+        #     return head.next
+        # front = head
+        # for i in range(count - n - 1) :
+        #     front = front.next
+        # front.next = front.next.next
+        # return head
+
+
+        ## Approach - Optimal 
+        if not head.next and n == 1 :
             return None
-        elif count == n :
+        slow , fast = head , head
+        for i in range(n) :
+            fast = fast.next
+        if not fast :
             return head.next
-        front = head
-        for i in range(count - n - 1) :
-            front = front.next
-        front.next = front.next.next
+        while fast and fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+
         return head
 
         
