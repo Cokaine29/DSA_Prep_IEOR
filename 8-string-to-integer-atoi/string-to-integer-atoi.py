c916e87cdc245232247bef39da5d5281a014fa69
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-27 07:23:32 +0530

    Time: 3 ms (35.29%) | Memory: 19.2 MB (78.90%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,34 +1,36 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        i = 0
-        n = len(s)
-
-        # Skip leading whitespaces
-        while i < n and s[i] == " ":
-            i += 1
-
-        # Handle sign
-        sign = 1
-        if i < n and (s[i] == '+' or s[i] == '-'):
-            if s[i] == '-':
-                sign = -1
-            i += 1
-
-        # Read digits
-        num = 0
-        while i < n and s[i].isdigit():
-            num = num * 10 + int(s[i])
-            i += 1
-
-        num *= sign
-
-        # Clamp to 32-bit signed integer range
         INT_MIN = -2**31
-        INT_MAX = 2**31 - 1
+        INT_MAX = 2**31 - 1 
+        ans = 0
+        digit = False 
+        sign = 1 
+        sign_done = False
+        for ele in s :
+            if not ele.isnumeric() :
+                if ele == " " and not digit and not sign_done:
+                    pass
+                elif ele == "-" and not digit and not sign_done:
+                    sign = -1 
+                    sign_done = True
+                elif ele == "+" and not digit and not sign_done:
+                    sign = +1
+                    sign_done = True
+                else :
+                    break
+            else :
+                digit = True
+                ans = ans * 10 + int(ele) 
+                
+        ans = sign * ans 
+        if ans < INT_MIN :
+            ans = INT_MIN
+        elif ans > INT_MAX :
+            ans = INT_MAX
+        
+        return ans
+                
+               
 
-        if num < INT_MIN:
-            return INT_MIN
-        if num > INT_MAX:
-            return INT_MAX
-
-        return num
+            
+        
