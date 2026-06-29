0f34dd2a2ed96c247e4d962fa1e594bf44502e41
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-06-29 18:56:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (37.90%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,22 +3,49 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        ## Approach 1 
+        # i = 0 
+        # j = 0 
+        # total = m + n 
 
-        i = 0 
-        j = 0 
-        total = m + n 
-
-        while j < n :
-            while i<m and j<n and nums1[i] < nums2[j] :
-                i += 1
-            # print((i,j))
-            k = total - 1 
-            while k > i:
-                nums1[k] = nums1[k-1]
-                # print(nums1)
-                k -= 1
-            nums1[i] = nums2[j]
-            # print(nums1)
-            m += 1 
-            j += 1
+        # while j < n :
+        #     while i<m and j<n and nums1[i] < nums2[j] :
+        #         i += 1
+        #     # print((i,j))
+        #     k = total - 1 
+        #     while k > i:
+        #         nums1[k] = nums1[k-1]
+        #         # print(nums1)
+        #         k -= 1
+        #     nums1[i] = nums2[j]
+        #     # print(nums1)
+        #     m += 1 
+        #     j += 1
         
+
+        ## Approach 2 
+
+        # if m == 0 and n > 0 :
+        #     for i in range(n) :
+        #         nums1[i] = nums2[i]
+        #     return 
+        # elif n == 0 :
+        #     return  
+        i = m - 1
+        j = n - 1 
+        k = m + n - 1
+
+        while j > -1 and i > -1:
+            if nums1[i] > nums2[j] :
+                nums1[k] = nums1[i]
+                k -= 1 
+                i -= 1
+            else :
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+        while  j > -1 :
+            nums1[k] = nums2[j] 
+            j -= 1 
+            k -= 1 
+     
