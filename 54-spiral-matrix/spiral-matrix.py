739f2e88b596d65d541d18d27ccb1d2faffffe3c
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-07-05 11:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (33.86%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -6,28 +6,56 @@ class Solution:
         left = 0
         right = n - 1
         ans = []
+        ## Method 1
+        # def spiral(matrix,top,bottom,left,right,ans) :
+        #     if left <= right :
+        #         for i in range(left,right+1) :
+        #             ans.append(matrix[top][i])
+        #         top += 1
+        #     if top <= bottom :
+        #         for i in range(top,bottom+1) :
+        #             ans.append(matrix[i][right])
+        #         right -= 1
+        #     if left <= right and top <= bottom:
+        #         for i in range(right,left - 1,-1) :
+        #             ans.append((matrix[bottom][i]))
+        #         bottom -= 1
+        #     if top <= bottom and left <= right:
+        #         for i in range(bottom, top - 1,-1) :
+        #             ans.append(matrix[i][left])
+        #         left += 1
+        #     return top,bottom,left,right,ans
+        # while left <= right and top <= bottom : 
+        #     top,bottom,left,right,ans = spiral(matrix,top,bottom,left,right,ans)
+        # return ans
 
-        def spiral(matrix,top,bottom,left,right,ans) :
-            if left <= right :
-                for i in range(left,right+1) :
-                    ans.append(matrix[top][i])
-                top += 1
+
+        ## Method 2 
+
+        while top <= bottom and left <= right :
+            # top left --> top right 
+            for i in range(left,right+1) :
+                ans.append(matrix[top][i])
+            top += 1
+            # top right --> bottom right
             if top <= bottom :
                 for i in range(top,bottom+1) :
                     ans.append(matrix[i][right])
-                right -= 1
-            if left <= right and top <= bottom:
-                for i in range(right,left - 1,-1) :
-                    ans.append((matrix[bottom][i]))
+                right -= 1 
+            # bottom right --> bottom left
+            if left <= right and top <= bottom :
+                for i in range(right,left-1,-1) :
+                    ans.append(matrix[bottom][i])
                 bottom -= 1
-            if top <= bottom and left <= right:
-                for i in range(bottom, top - 1,-1) :
-                    ans.append(matrix[i][left])
+            # bottom left --> top left
+            if left <= right and top <= bottom :
+                for i in range(bottom,top-1,-1) :
+                    ans.append(matrix[i][left]) 
                 left += 1
-            return top,bottom,left,right,ans
-        while left <= right and top <= bottom : 
-            top,bottom,left,right,ans = spiral(matrix,top,bottom,left,right,ans)
+
+
         return ans
+
 
 
 
