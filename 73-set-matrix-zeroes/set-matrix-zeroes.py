f74cc1425c1b39b9e5dce39f35a630f0f99dff2a
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-07-05 17:41:00 +0530

    Time: 295 ms (7.84%) | Memory: 20.2 MB (61.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,19 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        track = []
         for i in range(len(matrix)) :
             for j in range(len(matrix[0])) :
                 if matrix[i][j] == 0 :
-                    track.append([i,j])
-        for ele in track :
-            [i,j] = ele
-            print([i,j])
-            for k in range(len(matrix)) :
-                matrix[k][j] = 0
-            for k in range(len(matrix[0])) :
-                matrix[i][k] = 0
-            
-        return     
+                    for k in range(len(matrix)) :
+                        if matrix[k][j] == 0 :
+                            pass
+                        else :
+                            matrix[k][j] = "s" 
+                    for k in range(len(matrix[0])) :
+                        if matrix[i][k] == 0 :
+                            pass
+                        else :
+                            matrix[i][k] = "s"
+        for i in range(len(matrix)) :
+            for j in range(len(matrix[0])) :
+                if matrix[i][j] == "s" :
+                    matrix[i][j] = 0
+        return
+                    
+
         
-        
+      
