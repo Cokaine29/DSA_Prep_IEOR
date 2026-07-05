9d881a3bee11544e24f9d37f288534574e340dc4
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-07-05 18:18:24 +0530

    Time: 7 ms (58.96%) | Memory: 20.2 MB (61.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -25,22 +25,54 @@ class Solution:
 
         ## Better
 
-        row = []
-        col = []
+        # row = []
+        # col = []
 
+        # for i in range(len(matrix)) :
+        #     for j in range(len(matrix[0])) :
+        #         if matrix[i][j] == 0 :
+        #             row.append(i)
+        #             col.append(j)
+
+        # for ele in row :
+        #     matrix[ele][:] = [0] * len(matrix[0])
+        # for ele in col :
+        #     for j in range(len(matrix)) :
+        #         matrix[j][ele] = 0
+        # return
+                    
+        ## Optimal 
+        row0 = False
+        col0 = False
         for i in range(len(matrix)) :
             for j in range(len(matrix[0])) :
                 if matrix[i][j] == 0 :
-                    row.append(i)
-                    col.append(j)
+                    if i == 0 :
+                        row0 = True
+                    if j == 0 :
+                        col0 = True
 
-        for ele in row :
-            matrix[ele][:] = [0] * len(matrix[0])
-        for ele in col :
-            for j in range(len(matrix)) :
-                matrix[j][ele] = 0
+                    matrix[0][j] = "a"
+                    matrix[i][0] = "a"
+        
+        for k in range(1,len(matrix)) :
+            if matrix[k][0] == "a" :
+                matrix[k][:] = [0] * len(matrix[0])
+        for k in range(1,len(matrix[0])) :
+            if matrix[0][k] == "a" :
+                for m in range(len(matrix)) :
+                    matrix[m][k]= 0
+
+        if row0 :
+            matrix[0] = [0] * len(matrix[0])
+        if col0 :
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
         return
-                    
+
+
+
+
  
         
       
