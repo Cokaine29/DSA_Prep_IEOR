43b17c900c60b5c2d2a7d37f51f25ec30cb6199d
Niraj Prashant Kamble <88840617+Cokaine29@users.noreply.github.com>
2026-07-27 22:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.57%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -5,33 +5,59 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head or not head.next:
+        if not head or not head.next or k == 0 :
             return head
-        H = head
-        temp = head 
-        hold = head
+        # H = head
+        # temp = head 
+        # hold = head
 
-        f = head
-        count = 0 
-        while f :
-            count += 1
-            f = f.next
-        k = k % count 
+        # f = head
+        # count = 0 
+        # while f :
+        #     count += 1
+        #     f = f.next
+        # k = k % count 
 
-        while k > 0 :
-            while temp and temp.next and temp.next.next : 
-                temp = temp.next
+        # while k > 0 :
+        #     while temp and temp.next and temp.next.next : 
+        #         temp = temp.next
             
 
-            hold = temp.next
-            temp.next = None
-            hold.next = H
+        #     hold = temp.next
+        #     temp.next = None
+        #     hold.next = H
 
-            H = hold
-            temp = hold
-            k -= 1
+        #     H = hold
+        #     temp = hold
+        #     k -= 1
 
-        return hold
+        # return hold
+
+
+        count = 0
+        F = head
+        hold = head
+        temp = head
+        while temp :
+            count += 1 
+            temp = temp.next
+        k = k % count
+        if k == 0 :
+            return head
+        for i in range(count-k-1) : 
+            hold = hold.next 
+        H = hold.next
+        ans = hold.next
+        hold.next = None
+        while H and H.next :
+            H = H.next
+        if H :
+            H.next = F 
+
+        return ans
+
+
+
 
 
 
